8280afd5b08cc4325477928b4151e9195839515f
tmoritaa <tatsuya.ahad@gmail.com>
2018-04-20 21:51:47 -0400

    Initial commit

---
+++ Assets/ObjectFollower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour {
+    [SerializeField]
+    private GameObject objToFollow;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = this.transform.position;
+        pos = new Vector3(objToFollow.transform.position.x, objToFollow.transform.position.y, pos.z);
+        this.transform.position = pos;
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    [SerializeField]
+    private float moveMag = 100f;
+
+    private Rigidbody2D body;
+
+    private Vector2 curDir = new Vector2();
+
+    void Awake() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+        Vector2 newDir = new Vector2();
+
+		if (Input.GetKey(KeyCode.W)) {
+            newDir.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            newDir.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            newDir.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            newDir.x += 1;
+        }
+
+        curDir = newDir.normalized;
+	}
+
+    void FixedUpdate() {
+        body.velocity = curDir * moveMag;
+    }
+}
