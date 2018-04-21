90eb09b48f187844093e68909bfdb352c6bf1054
tmoritaa <tatsuya.ahad@gmail.com>
2018-04-21 14:37:23 -0400

    Implemented time stop logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,14 +8,35 @@ public class Player : GameEntity {
     [SerializeField]
     private float moveMag = 100f;
 
+    [SerializeField]
+    private float timeSlowdownVal = 0.33f;
+
+    [SerializeField]
+    private float timeBarDepletionPerSec = 33;
+
+    [SerializeField]
+    private float timeBarRechargePerSec = 25;
+
+    [SerializeField]
+    private float minTimeBarRequiredForActivation = 20;
+
+    [SerializeField]
+    private float maxTimeBar = 100f;
+
     private Rigidbody2D body;
 
     private Vector2 curDir = new Vector2();
+
+    private bool timeStopped = false;
+
+    private float curTimeBar = 0;
 
     protected override void Awake() {
         base.Awake();
 
         body = GetComponent<Rigidbody2D>();
+
+        curTimeBar = maxTimeBar;
     }
 
     void Update () {
@@ -36,6 +57,8 @@ public class Player : GameEntity {
         }
 
         curDir = newDir.normalized;
+
+        handleTimeStopLogic();
 	}
 
     void FixedUpdate() {
@@ -50,5 +73,27 @@ public class Player : GameEntity {
     protected override void onDeath() {
         Debug.Log("Player dead");
         // TODO: for now do nothing. Later display game over screen
+    }
+
+    private void handleTimeStopLogic() {
+        if (!timeStopped && curTimeBar >= minTimeBarRequiredForActivation && Input.GetKeyDown(KeyCode.Space)) {
+            timeStopped = true;
+        } else if (timeStopped && Input.GetKeyUp(KeyCode.Space)) {
+            timeStopped = false;
+        }
+
+        if (timeStopped) {
+            curTimeBar = Mathf.Clamp(curTimeBar - timeBarDepletionPerSec * Time.unscaledDeltaTime, 0, maxTimeBar);
+        } else {
+            curTimeBar = Mathf.Clamp(curTimeBar + timeBarRechargePerSec * Time.unscaledDeltaTime, 0, maxTimeBar);
+        }
+
+        if (curTimeBar == 0) {
+            timeStopped = false;
+        }
+
+        Time.timeScale = timeStopped ? timeSlowdownVal : 1;
+
+        Debug.Log(curTimeBar);
     }
 }
